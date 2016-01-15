a5e03bb349b70ff1f755e314fe21714053622cea
Maryaeria <maryamsyed2096@gmail.com>
2016-01-14 16:00:47 -0800

    Separate the gallery and tab sections of index.html into new directives for level04 challenges

--- challenges04/app.js
+++ challenges04/app.js
@@ -5,26 +5,6 @@
     this.products = gems;
   });
 
-  app.controller('TabController', function() {
-      this.tab = 1;
-
-      this.setTab = function(setTab) {
-          this.tab = setTab;
-      };
-
-      this.isSet = function(isTab) {
-          return this.tab === isTab;
-      };
-  });
-
-  app.controller('GalleryController', function() {
-      this.current = 0;
-
-     this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
   app.controller('ReviewController', function() {
        this.review = {};
 
@@ -46,6 +26,40 @@
       return {
           restrict: 'A',
           templateUrl: 'product-specs.html'
+      };
+  });
+
+  app.directive('productTabs', function(){
+      return {
+          restrict: 'E',
+          templateUrl: 'product-tabs.html',
+          controller: function() {
+            this.tab = 1;
+
+            this.setTab = function(setTab) {
+            this.tab = setTab;
+            };
+
+            this.isSet = function(isTab) {
+            return this.tab === isTab;
+      };
+    },
+    controllerAs: 'tab'
+
+      };
+  });
+
+  app.directive('productGallery', function(){
+      return {
+          restrict: 'E',
+          templateUrl: 'product-gallery.html',
+          controller: function() {
+            this.current = 0;
+            this.setCurrent = function(newGallery){
+            this.current = newGallery || 0;
+            };
+          },
+          controllerAs: 'gallery'
       };
   });
 
